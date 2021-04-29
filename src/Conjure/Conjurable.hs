016dc580e94fb69591060b280c5b4b03819a20a3
Rudy Matela <rudy@matela.com.br>
2021-04-29 11:55:03 -0300

    add more Conjurable functions

--- src/Conjure/Conjurable.hs
+++ src/Conjure/Conjurable.hs
@@ -11,7 +11,10 @@
 --
 -- You are probably better off importing "Conjure".
 module Conjure.Conjurable
-  ( Conjurable
+  ( Reification1
+  , Reification
+  , Conjurable (..)
+  , conjureType
   , canonicalApplication
   , canonicalVarApplication
   , unifiedArgumentTiers
@@ -25,25 +28,119 @@ import Test.LeanCheck.Utils
 import Conjure.Expr hiding (application)
 import Test.Speculate.Expr
 
+
+-- | Single reification of some functions over a type as 'Expr's.
+--
+-- A hole, an if function, an equality function and tiers.
+type Reification1  =  (Expr, Expr, Maybe Expr, Maybe [[Expr]])
+
+-- | A reification over a collection of types.
+--
+-- Represented as a transformation of a list to a list.
+type Reification  =  [Reification1] -> [Reification1]
+
+
 class Typeable a => Conjurable a where
   argumentHoles :: a -> [Expr]
   argumentHoles _  =  []
+
   argumentTiers :: a -> [ [[Expr]] ]
   argumentTiers _  =  []
 
-instance Conjurable ()
-instance Conjurable Int
-instance Conjurable Integer
-instance Conjurable Bool
-instance Typeable a => Conjurable [a]
-instance (Typeable a, Typeable b) => Conjurable (a,b)
-instance Typeable a => Conjurable (Maybe a)
-instance (Typeable a, Typeable b) => Conjurable (Either a b)
+  conjureEquality :: a -> Maybe Expr
+  conjureEquality _  =  Nothing
 
-instance Conjurable Float
-instance Conjurable Double
-instance Conjurable Ordering
+  conjureTiers :: a -> Maybe [[Expr]]
+  conjureTiers _  =  Nothing
 
+  conjureSubTypes :: a -> Reification
+  conjureSubTypes _  =  id
+
+
+conjureType :: Conjurable a => a -> Reification
+conjureType x ms  =
+  if hole x `elem` [h | (h,_,_,_) <- ms]
+  then ms
+  else conjureSubTypes x $ (hole x, ifFor x, conjureEquality x, conjureTiers x) : ms
+
+reifyEquality :: (Eq a, Typeable a) => a -> Maybe Expr
+reifyEquality  =  Just . head . reifyEq
+
+reifyTiers :: (Listable a, Show a, Typeable a) => a -> Maybe [[Expr]]
+reifyTiers  =  Just . mkExprTiers
+
+mkExprTiers :: (Listable a, Show a, Typeable a) => a -> [[Expr]]
+mkExprTiers a  =  mapT val (tiers -: [[a]])
+
+conjureTiersFor :: Conjurable f => f -> Expr -> [[Expr]]
+conjureTiersFor f e  =  tf allTiers
+  where
+  allTiers :: [ [[Expr]] ]
+  allTiers  =  mkExprTiers (undefined :: Bool)
+            :  argumentTiers f
+  tf []  =  [[e]] -- no tiers found, keep variable
+  tf (etiers:etc)  =  case etiers of
+                      ((e':_):_) | typ e' == typ e -> etiers
+                      _                            -> tf etc
+
+
+instance Conjurable () where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+
+instance Conjurable Int where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+
+instance Conjurable Integer where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+
+instance Conjurable Bool where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+
+-- TODO: remove Eq restriction here and throughout
+instance (Conjurable a, Listable a, Show a, Eq a) => Conjurable [a] where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+  conjureSubTypes xs  =  conjureType (head xs)
+
+
+instance ( Conjurable a, Listable a, Show a, Eq a
+         , Conjurable b, Listable b, Show b, Eq b
+         ) => Conjurable (a,b) where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+  conjureSubTypes xy  =  conjureType (fst xy)
+                      .  conjureType (snd xy)
+
+instance (Conjurable a, Listable a, Show a, Eq a) => Conjurable (Maybe a) where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+  conjureSubTypes xs  =  conjureType (fromJust xs)
+
+instance ( Conjurable a, Listable a, Show a, Eq a
+         , Conjurable b, Listable b, Show b, Eq b
+         ) => Conjurable (Either a b) where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+  conjureSubTypes xs  =  conjureType (fromLeft xs)
+                      .  conjureType (fromRight xs)
+
+instance Conjurable Float where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+
+instance Conjurable Double where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+
+instance Conjurable Ordering where
+  conjureEquality  =  reifyEquality
+  conjureTiers     =  reifyTiers
+
+-- TODO: complete the following instances
 instance (Typeable a, Typeable b, Typeable c) => Conjurable (a,b,c)
 instance (Typeable a, Typeable b, Typeable c, Typeable d) => Conjurable (a,b,c,d)
 instance (Typeable a, Typeable b, Typeable c, Typeable d, Typeable e) => Conjurable (a,b,c,d,e)
@@ -77,9 +174,6 @@ nubArgumentTiers  =  nubOn tierepr . argumentTiers
   nubOn f  =  nubBy ((==) `on` f)
   -- NOTE: this is O(n*n),
   -- not much of a problem the number of arguments will hardly pass 6.
-
-mkExprTiers :: (Listable a, Show a, Typeable a) => a -> [[Expr]]
-mkExprTiers a  =  mapT val (tiers -: [[a]])
 
 tiersFor :: Conjurable f => f -> Expr -> [[Expr]]
 tiersFor f e  =  tf (mkExprTiers (undefined :: Bool) : argumentTiers f)
