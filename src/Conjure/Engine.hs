ed8368d61d12a1a671ac3c7809ca3a1f4eacc0e6
Rudy Matela <rudy@matela.com.br>
2021-04-30 14:03:43 -0300

    code-golf

--- src/Conjure/Engine.hs
+++ src/Conjure/Engine.hs
@@ -147,10 +147,10 @@ conjure  =  conjureWith args
 --
 -- > conjureWithMaxSize 10 "function" function [...]
 conjureWithMaxSize :: Conjurable f => Int -> String -> f -> [Expr] -> IO ()
-conjureWithMaxSize sz  =  conjureWith as
-  where
-  as | maxEquationSize args <= sz  =  args{maxSize = sz}
-     | otherwise                   =  args{maxSize = sz, maxEquationSize = sz}
+conjureWithMaxSize sz  =  conjureWith args
+                       {  maxSize = sz
+                       ,  maxEquationSize = min sz (maxEquationSize args)
+                       }
 
 -- | Like 'conjure' but allows setting options through 'Args'/'args'.
 --
