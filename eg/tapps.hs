b20e9f0640fa506451ecb2881b7e83645595676f
Rudy Matela <rudy@matela.com.br>
2021-05-03 16:52:39 -0300

    eg/tapps: portable TypeApplications
    what I noted on the message of commit 67d93a5 is not true,
    TypeApplications do not really work down to GHC 7.8 as
    they were only introduced on GHC 8.0:
    
    https://gitlab.haskell.org/ghc/ghc/-/wikis/language-pragma-history
    
    My prediction on 37a30f3 is actually true.
    
    See the notes in the modified file for details.

--- eg/tapps.hs
+++ eg/tapps.hs
@@ -1,8 +1,12 @@
--- tapps.hs: conjure with type applications
+-- tapps.hs: conjure with (portable) type applications
 --
 -- Copyright (C) 2021 Rudy Matela
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
+{-# LANGUAGE CPP #-}
+#if __GLASGOW_HASKELL__ < 800
+#else
 {-# LANGUAGE TypeApplications #-}
+#endif
 
 import Conjure
 
@@ -25,21 +29,53 @@ primitives :: [Expr]
 primitives =
   [ val (0 :: Int)
   , val (1 :: Int)
+#if __GLASGOW_HASKELL__ < 800
+  , value "+" ((+) :: Int -> Int -> Int)
+  , value "*" ((*) :: Int -> Int -> Int)
+  , value "null" (null :: [Int] -> Bool)
+  , value "head" (head :: [Int] -> Int)
+  , value "tail" (tail :: [Int] -> [Int])
+#else
   , value "+" ((+) @Int)
   , value "*" ((*) @Int)
+-- the following #if was added just for dramatic effect (see notes below)
+#if __GLASGOW_HASKELL__ < 710
+  , value "null" (null @Int)
+#else
   , value "null" (null @[] @Int)
+#endif
   , value "head" (head @Int)
   , value "tail" (tail @Int)
+#endif
   ]
 
 primitivesWithFold :: [Expr]
+#if __GLASGOW_HASKELL__ < 800
+primitivesWithFold  =
+    value "foldr" (foldr :: (Int -> Int -> Int) -> Int -> [Int] -> Int)
+  : primitives
+#else
+-- the following #if was added just for dramatic effect (see notes below)
+#if __GLASGOW_HASKELL__ < 710
+primitivesWithFold  =  value "foldr" (foldr @Int @Int) : primitives
+#else
 primitivesWithFold  =  value "foldr" (foldr @[] @Int @Int) : primitives
+#endif
+#endif
 
 -- Some notes:
 --
--- This works down to GHC 7.8 even though at the time (base 4.7.0.2) null and
--- foldr were bound to the list type.
+-- From GHC 7.8 to GHC 7.10,
+-- null, foldr and other functions were generalized
+-- from lists to foldable containers.
+-- The type application for the old version of null
+-- is different than the one for the new version.
 --
--- base for GHC 7.8:  https://hackage.haskell.org/package/base-4.7.0.2/docs/Prelude.html#v:null
--- base for GHC 7.10: https://hackage.haskell.org/package/base-4.8.0.0/docs/Prelude.html#v:null
--- working run:       https://github.com/rudymatela/conjure/runs/2493232850
+-- In the case of this file this is not really an issue
+-- as the above change was introduced before TypeApplications even existed.
+--
+-- This illustrates that
+-- we may have to use a different type application patterns
+-- when using different versions of libraries.
+-- In some cases, code using TypeApplications may be
+-- less portable than it's full type binding counterparts.
