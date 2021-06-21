077d821f675ebac65199b777fb1f22507d17c5c7
Rudy Matela <rudy@matela.com.br>
2021-06-21 09:33:01 -0300

    Add replicates to replicate

--- eg/replicate.hs
+++ eg/replicate.hs
@@ -3,7 +3,7 @@
 -- Copyright (C) 2021 Rudy Matela
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import Conjure
-import Data.List (insert)
+import Data.List (transpose)
 
 replicate' :: Int -> Char -> String
 replicate' 0 c  =  []
@@ -11,6 +11,16 @@ replicate' 1 c  =  [c]
 replicate' 2 c  =  [c,c]
 replicate' 3 c  =  [c,c,c]
 replicate' 4 c  =  [c,c,c,c]
+
+replicates' :: String -> Int -> String
+replicates' [a]     1  =  [a]
+replicates' [a,b]   1  =  [a,b]
+replicates' [a]     2  =  [a,a]
+replicates' [a,b]   2  =  [a,a,b,b]
+replicates' [a,b,c] 2  =  [a,a,b,b,c,c]
+replicates' [a]     3  =  [a,a,a]
+replicates' [a,b]   3  =  [a,a,a,b,b,b]
+replicates' [a,b,c] 3  =  [a,a,a,b,b,b,c,c,c]
 
 main :: IO ()
 main = do
@@ -21,3 +31,22 @@ main = do
     , val ""
     , value ":" ((:) :: Char -> String -> String)
     ]
+
+  conjureWith args{maxTests=360} "replicates" replicates'
+    [ value "replicate" (replicate :: Int -> String -> [String])
+    , value "transpose" (transpose :: [[Char]] -> [[Char]])
+    , value "concat"    (concat :: [String] -> String)
+    ]
+
+  conjureWith args{maxTests=360, maxSize=13} "replicates" replicates'
+    [ val ""
+    , value "null" (null :: String -> Bool)
+    , value "head" (head :: String -> Char)
+    , value "tail" (tail :: String -> String)
+    , value ":" ((:) :: Char -> String -> String)
+    , value "++" ((++) :: String -> String -> String)
+    , value "replicate" (replicate :: Int -> Char -> String)
+    ]
+
+replicates n []  =  []
+replicates n (x:xs)  =  replicate n x ++ replicates n xs
