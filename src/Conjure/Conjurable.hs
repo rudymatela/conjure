840434341277d52f0524fe323aebaaae46410ac4
Rudy Matela <rudy@matela.com.br>
2021-07-23 11:18:13 -0300

    change concept of size for Ratios

--- src/Conjure/Conjurable.hs
+++ src/Conjure/Conjurable.hs
@@ -524,7 +524,7 @@ instance (Integral a, Conjurable a, Listable a, Show a, Eq a, Express a) => Conj
   conjureExpress   =  reifyExpress
   conjureEquality  =  reifyEquality
   conjureTiers     =  reifyTiers
-  conjureSize      =  round
+  conjureSize q    =  fromIntegral $ numerator q + denominator q
   conjureSubTypes q  =  conjureType (numerator q)
 
 instance (RealFloat a, Conjurable a, Listable a, Show a, Eq a, Express a) => Conjurable (Complex a) where
