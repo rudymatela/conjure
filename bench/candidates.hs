a27ec8f30f3fea46d851227246269ace4c54da6a
Rudy Matela <rudy@matela.com.br>
2021-07-23 12:49:01 -0300

    bench/candidates: record more candidates

--- bench/candidates.hs
+++ bench/candidates.hs
@@ -41,7 +41,7 @@ main  =  do
     , prim "dec" (subtract 1 :: Int -> Int)
     ]
 
-  printCandidates 8 4 "?" (undefined :: Int -> Int -> Int)
+  printCandidates 9 6 "?" (undefined :: Int -> Int -> Int)
     [ pr (0 :: Int)
     , prim "+" ((+) :: Int -> Int -> Int)
     , prim "*" ((+) :: Int -> Int -> Int)
