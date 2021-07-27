e46d9ff6418f30028de1e5431f758baf2c8d03a2
Rudy Matela <rudy@matela.com.br>
2021-07-27 13:55:40 -0300

    test/defn: add and1Defn and or1Defn

--- test/defn.hs
+++ test/defn.hs
@@ -25,6 +25,14 @@ tests n  =
   , dvl orDefn  (orV  :$ val [False,True])      == True
   , dvl orDefn  (orV  :$ val [True,True])       == True
   , dvl orDefn  (orV  :$ val [True,False,True]) == True
+  , dvl and1Defn (andV :$ val [False,False])     == False
+  , dvl and1Defn (andV :$ val [False,True])      == False
+  , dvl and1Defn (andV :$ val [True,True])       == True
+  , dvl and1Defn (andV :$ val [True,False,True]) == False
+  , dvl or1Defn  (orV  :$ val [False,False])     == False
+  , dvl or1Defn  (orV  :$ val [False,True])      == True
+  , dvl or1Defn  (orV  :$ val [True,True])       == True
+  , dvl or1Defn  (orV  :$ val [True,False,True]) == True
 
   , dvl factDefn (factV :$ val (0 :: Int)) == (1 :: Int)
   , dvl factDefn (factV :$ val (1 :: Int)) == (1 :: Int)
@@ -96,6 +104,14 @@ orDefn :: Defn
 orDefn  =  [ or' nilBool       =-  false
            , or' (pp -:- pps)  =-  pp -||- (orV :$ pps)
            ]  where or' e  =  orV :$ e
+
+and1Defn :: Defn
+and1Defn  =  [ and' pps  =-  null' pps -||- head' pps -&&- and' (tail' pps)
+             ]  where  and' e  =  andV :$ e
+
+or1Defn :: Defn
+or1Defn  =  [ or' pps  =-  not' (null' pps) -&&- (head' pps -||- or' (tail' pps))
+            ]  where or' e  =  orV :$ e
 
 (=-) = (,)
 infixr 0 =-
