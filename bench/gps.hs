9797665d2434ad843266a65a050e110738375e4d
Rudy Matela <rudy@matela.com.br>
2021-07-31 16:26:53 -0300

    add gps3

--- bench/gps.hs
+++ bench/gps.hs
@@ -4,6 +4,7 @@
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import Conjure
 
+
 gps1p :: Int -> Float -> Float
 gps1p 0 1.0  =  1.0
 gps1p 1 0.0  =  1.0
@@ -18,6 +19,7 @@ gps1c  =  conjure "gps1" gps1p
   [ prim "+" ((+) :: Float -> Float -> Float)
   , prim "fromIntegral" (fromIntegral :: Int -> Float)
   ]
+
 
 gps2p :: Int -> Maybe String
 gps2p    0  =  Just "small"
@@ -46,7 +48,39 @@ gps2c  =  conjureWith args{maxTests=5080, maxSize=30} "gps2" gps2p
   , prif (undefined :: Maybe String)
   ]
 
+
+gps3p :: Int -> Int -> Int -> [Int]
+gps3p 0 9 1  =  [0,1,2,3,4,5,6,7,8]
+gps3p 2 9 2  =  [2,4,6,8]
+
+gps3g1 :: Int -> Int -> Int -> [Int]
+gps3g1 start end step  =  enumFromThenTo start (step+start) (end-1)
+
+gps3g2 :: Int -> Int -> Int -> [Int]
+gps3g2 start end step  =  if start < end
+                          then start : gps3g2 (start+step) end step
+                          else []
+
+gps3c :: IO ()
+gps3c  =  do
+  conjure "gps3" gps3p
+    [ pr (1 :: Int)
+    , prim "enumFromThenTo" ((\x y z -> take 360 $ enumFromThenTo x y z) :: Int -> Int -> Int -> [Int])
+    , prim "+" ((+) :: Int -> Int -> Int)
+    , prim "-" ((-) :: Int -> Int -> Int)
+    ]
+
+  -- not possible, no recursive descent
+  conjureWith args{maxSize=8} "gps3" gps3p
+    [ pr ([] :: [Int])
+    , prim ":" ((:) :: Int -> [Int] -> [Int])
+    , prim "+" ((+) :: Int -> Int -> Int)
+    , prim "<" ((<) :: Int -> Int -> Bool)
+    , prif (undefined :: [Int])
+    ]
+
 main :: IO ()
 main  =  do
   gps1c
   gps2c
+  gps3c
