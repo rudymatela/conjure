93c1484804f02d6d9a26b803497659c7f8fc4eeb
Rudy Matela <rudy@matela.com.br>
2021-08-12 13:09:19 -0300

    add gps1p2

--- bench/gps2.hs
+++ bench/gps2.hs
@@ -18,6 +18,21 @@ gps1p [1,-1,-1]  =  Just 2
 gps1g :: [Int] -> Maybe Int
 gps1g xs  =  findIndex (0 >) (map sum (tail (inits xs)))
 
+gps1p2 :: Int -> [Int] -> Int
+gps1p2 0 [0,-1,2]   =  1
+gps1p2 0 [-1,0,1]   =  0
+gps1p2 0 [0,-1]     =  1
+gps1p2 0 [1,-1,-1]  =  2
+gps1p2 0 [0,0,0,-1]  =  3
+gps1p2 0 [1,0,0,-2]  =  3
+
+-- efficient gps1, unreachable performance-wise
+gps1g2 :: Int -> [Int] -> Int
+gps1g2 t []  =  undefined -- 1
+gps1g2 t (x:xs)  =  if t + x < 0 -- 7
+                    then 0 -- 8
+                    else 1 + gps1g2 (t + x) xs -- 15
+
 gps1c :: IO ()
 gps1c  =  do
   conjure "gps1" gps1p
@@ -42,6 +57,15 @@ gps1c  =  do
     , prim "map" (map :: ([Int] -> Int) -> [[Int]] -> [Int])
     , prim "inits" (inits :: [Int] -> [[Int]])
     , prim "tail" (tail :: [[Int]] -> [[Int]])
+    ]
+
+  conjureWithMaxSize 4 "gps1" gps1p2
+    [ pr (0 :: Int)
+    , pr (1 :: Int)
+    , prim "+" ((+) :: Int -> Int -> Int)
+    , prim "<" ((<) :: Int -> Int -> Bool)
+    , prif (undefined :: Int)
+    , prim "undefined" (undefined :: Int)
     ]
 
 
