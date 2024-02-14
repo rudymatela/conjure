7cecc263e27594743380d53c9100edea6ad3c2d5
Rudy Matela <rudy@matela.com.br>
2024-02-14 17:56:07 +0100

    fix program title

--- bench/strategies.hs
+++ bench/strategies.hs
@@ -1,4 +1,4 @@
--- carryon.hs: conjuring implementations of a factorial function
+-- strategies.hs: conjuring factorial with different strategies
 --
 -- Copyright (C) 2021-2024 Rudy Matela
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
