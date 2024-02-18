422f11af0cdd688a316d63b67d996ee06cfca8c0
Rudy Matela <rudy@matela.com.br>
2024-02-18 11:17:16 +0100

    Update Colin's IntFuns to recent Conjure interface

--- eg/colin/IntFuns.hs
+++ eg/colin/IntFuns.hs
@@ -1,4 +1,5 @@
 -- 2021  Colin Runciman
+-- Updated in 2024 to use the new Conjure interface
 import Conjure
 import Prelude hiding (exp)
 
@@ -11,12 +12,12 @@ tri 5 = 15
 -- hoping for something like
 -- tri x = if x==1 then 1 else x + tri (dec x)
 
-triBackground :: [Expr]
-triBackground =
-  [ value "==" ((==) :: Int -> Int -> Bool)
-  , val (1::Int)
-  , value "+" ((+) :: Int -> Int -> Int)
-  , value "dec" ((\x -> x-1) :: Int -> Int)
+triPrimitives :: [Prim]
+triPrimitives =
+  [ prim "==" ((==) :: Int -> Int -> Bool)
+  , pr (1::Int)
+  , prim "+" ((+) :: Int -> Int -> Int)
+  , prim "dec" ((\x -> x-1) :: Int -> Int)
   ]
 
 -- looking through 3039 candidates, 25% match, 1/4 assignments
@@ -33,12 +34,13 @@ fib 7 = 21
 -- hoping for something like
 -- fib x = if x <= 1 then 1 else fib (dec (dec x)) + fib (dec x)
 --         1  2 3  4      5      6    7    8   9   10 11  12  13
-fibBackground :: [Expr]
-fibBackground =
-  [ value "<=" ((<=) :: Int -> Int -> Bool)
-  , val (1::Int)
-  , value "+" ((+) :: Int -> Int -> Int)
-  , value "dec" ((\x -> x-1) :: Int -> Int)
+fibPrimitives :: [Prim]
+fibPrimitives =
+  [ prim "<=" ((<=) :: Int -> Int -> Bool)
+  , pr (0::Int)
+  , pr (1::Int)
+  , prim "+" ((+) :: Int -> Int -> Int)
+  , prim "dec" ((\x -> x-1) :: Int -> Int)
   ]
 
 -- looking through 23418 candidates
@@ -52,10 +54,10 @@ exp 5 = 3125
 
 -- hoping for
 -- exp x = product (replicate x x)
-expBackground :: [Expr]
-expBackground =
-  [ value "replicate" (replicate :: Int -> Int -> [Int])
-  , value "product" (product :: [Int] -> Int)
+expPrimitives :: [Prim]
+expPrimitives =
+  [ prim "replicate" (replicate :: Int -> Int -> [Int])
+  , prim "product" (product :: [Int] -> Int)
   ]
 
 -- looking through 18 candidates
@@ -72,12 +74,12 @@ pri 7 = True
 
 -- hoping for something like
 -- pri p = factors p == list2 1 p
-priBackground :: [Expr]
-priBackground =
-  [ value "==" ((==) :: [Int] -> [Int] -> Bool)
-  , value "factors" ((\n -> filter (\k -> n `mod` k == 0) [1..n]) :: Int -> [Int])
-  , value "list2" ((\i j -> [i,j]) :: Int -> Int -> [Int])
-  , val (1 :: Int)
+priPrimitives :: [Prim]
+priPrimitives =
+  [ prim "==" ((==) :: [Int] -> [Int] -> Bool)
+  , prim "factors" ((\n -> filter (\k -> n `mod` k == 0) [1..n]) :: Int -> [Int])
+  , prim "list2" ((\i j -> [i,j]) :: Int -> Int -> [Int])
+  , pr (1 :: Int)
   ]
 
 -- looking through 38 candidates, 100% match, 7/7 assignments
@@ -86,8 +88,8 @@ priBackground =
 
 main :: IO ()
 main = do
-  conjureWithMaxSize 10 "tri" tri triBackground
-  conjureWith args{maxSize=13, maxRecursiveCalls=2, maxRecursionSize=360} "fib" fib fibBackground
-  conjure "exp" exp expBackground
-  conjure "pri" pri priBackground
+  conjureWithMaxSize 10 "tri" tri triPrimitives
+  conjureWith args{maxSize=13} "fib" fib fibPrimitives
+  conjure "exp" exp expPrimitives
+  conjure "pri" pri priPrimitives
 
