4dbbd0bcbc6e1caaeb43c2853b22461cdf02aa1f
Rudy Matela <rudy@matela.com.br>
2025-02-05 16:44:15 +0100

    improve main documentation with another example

--- src/Conjure.hs
+++ src/Conjure.hs
@@ -43,9 +43,51 @@
 -- > factorial 0  =  1
 -- > factorial x  =  x * factorial (x - 1)
 --
+-- The above example takes less than a second to run in a modern laptop.
+--
 -- Factorial is discovered from scratch through a search.
 -- We prune the search space using properties discovered
 -- from the results of testing.
+--
+-- Conjure is not limited to integers,
+-- it works for functions over algebraic data types too.
+-- See:
+--
+-- > take' :: Int -> [a] -> [a]
+-- > take' 0 [x]    =  []
+-- > take' 1 [x]    =  [x]
+-- > take' 0 [x,y]  =  []
+-- > take' 1 [x,y]  =  [x]
+-- > take' 2 [x,y]  =  [x,y]
+-- > take' 3 [x,y]  =  [x,y]
+--
+-- > > conjure "take" (take' :: Int -> [A] -> [A])
+-- > >   [ pr (0 :: Int)
+-- > >   , pr (1 :: Int)
+-- > >   , pr ([] :: [A])
+-- > >   , prim ":" ((:) :: A -> [A] -> [A])
+-- > >   , prim "-" ((-) :: Int -> Int -> Int)
+-- > >   ]
+-- > take :: Int -> [A] -> [A]
+-- > -- testing 153 combinations of argument values
+-- > -- pruning with 4/7 rules
+-- > -- ...  ...  ...
+-- > -- looking through 58 candidates of size 9
+-- > -- tested 104 candidates
+-- > take 0 xs  =  []
+-- > take x []  =  []
+-- > take x (y:xs)  =  y:take (x - 1) xs
+--
+-- The above example also takes less than a second to run in a modern laptop.
+-- The selection of functions in the list of primitives was minimized
+-- to what was absolutely needed here.
+-- With a larger collection as primitives YMMV.
+--
+-- Conjure works for user-defined algebraic data types too,
+-- given that they are made instances of the 'Conjurable' typeclass.
+-- For types without data invariants,
+-- it should be enough to call 'deriveConjurable'
+-- to create an instance using TH.
 {-# LANGUAGE CPP #-}
 module Conjure
   (
