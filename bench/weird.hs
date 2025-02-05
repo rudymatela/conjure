d85a5a96449e3c0309b1d1d2bcea5b384dab98bd
Rudy Matela <rudy@matela.com.br>
2025-02-05 18:27:39 +0100

    add note in bench/weird

--- bench/weird.hs
+++ bench/weird.hs
@@ -5,6 +5,10 @@
 --
 -- This file conjures some weird or unintuitive functions.
 -- It is here to make sure we do not overprune!
+--
+-- For simplicity,
+-- we do not use partial definitions here.
+-- We use fully defined functions to conjure themselves.
 import Conjure
 
 -- | xor for integers
@@ -20,6 +24,11 @@ main  =  do
   conjure "^^^" (^^^) primitives
   conjureWith args{usePatterns = False} "^^^" (^^^) primitives
 
+  -- This example is quite degenerate,
+  -- it takes a while to conjure even with just 2 primitives.
+  -- I am leaving it commented-out for now...
+  -- conjure "thirty" thirty [pr (0::Int), prim "+" ((+) :: Int -> Int -> Int)]
+
 primitives :: [Prim]
 primitives  =
   [ pr (0::Int)
@@ -31,3 +40,12 @@ primitives  =
   , prim "||" (||)
   , prif (undefined :: Int)
   ]
+
+-- | returns the sum when one of the arguments is 0
+--
+-- Naming: thirty/30, sum of __3__, when one is __0__
+thirty :: Int -> Int -> Int -> Int
+thirty 0 y z  =  y + z
+thirty x 0 z  =  x + z
+thirty x y 0  =  x + y
+thirty x y z  =  0
