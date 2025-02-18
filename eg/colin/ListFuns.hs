3c84a6fa79a0c28693aed6275acd3c2a5577f588
Rudy Matela <rudy@matela.com.br>
2025-02-18 15:46:21 +0100

    update Colin's ListFuns example

--- eg/colin/ListFuns.hs
+++ eg/colin/ListFuns.hs
@@ -1,147 +1,153 @@
--- 2021  Colin Runciman
--- updated in 2024 to use the recent interface of Conjure
+--- 2021, 2025  Colin Runciman
 import Conjure
 import Prelude hiding (sum, take, drop)
 
-sumSpec :: ([Int] -> Int) -> Bool
-sumSpec sum  =  and [ sum []       ==  0
-                    , sum [0,1]    ==  1
-                    , sum [1,0,1]  ==  2 ]
+sum :: [Int] -> Int
+sum []       =  0
+sum [0,1]    =  1
+sum [1,0,1]  =  2
 
 -- hoping for something like
--- sum xs = if null xs then 0 else head xs + sum (tail xs)
+-- sum []      =  0
+-- sum (x:xs)  =  x + sum xs
 
-sumPrimitives :: [Prim]
-sumPrimitives =
-  [ prim "null" (null :: [Int] -> Bool)
-  , pr (0::Int)
-  , prim "+"    ((+) :: Int -> Int -> Int)
-  , prim "head" (head :: [Int] -> Int)
-  , prim "tail" (tail :: [Int] -> [Int])
+sumBackground :: [Prim]
+sumBackground =
+  [ pr (0::Int)
+  , prim "+" ((+) :: Int -> Int -> Int)
   ]
 
-appSpec :: ([Int] -> [Int] -> [Int]) -> Bool
-appSpec (++)  =  and [ []    ++ [0,1]  ==  [0,1]
-                     , [0,1] ++ []     ==  [0,1]
-                     , [0,1] ++ [0,1]  ==  [0,1,0,1] ]
+app :: [Int] -> [Int] -> [Int]
+app []      [0,1]  =  [0,1]
+app [0,1]   []     =  [0,1]
+app [0,1]   [0,1]  =  [0,1,0,1]
 
 -- hoping for something like
--- app xs ys = if null xs then ys else head xs : app (tail xs) ys
+-- app []     ys  =  ys
+-- app (x:xs) ys  =  x : app xs ys
 
-appPrimitives :: [Prim]
-appPrimitives =
-  [ prim "null" (null :: [Int] -> Bool)
-  , prim ":"    ((:) :: Int -> [Int] -> [Int])
-  , prim "head" (head :: [Int] -> Int)
-  , prim "tail" (tail :: [Int] -> [Int])
+appBackground :: [Prim]
+appBackground =
+  [ prim ":" ((:) :: Int -> [Int] -> [Int])  
   ]
 
-memSpec :: (Int -> [Int] -> Bool) -> Bool
-memSpec mem  =  and [ 0 `mem` []       ==  False
-                    , 0 `mem` [0,1,1]  ==  True
-                    , 0 `mem` [1,0,1]  ==  True
-                    , 0 `mem` [1,1,0]  ==  True
-                    , 0 `mem` [1,1,1]  ==  False ]
+mem :: Int -> [Int] -> Bool
+mem 0 []       =  False
+mem 0 [0,1,1]  =  True
+mem 0 [1,0,1]  =  True
+mem 0 [1,1,0]  =  True
+mem 0 [1,1,1]  =  False
 
 -- hoping for something like
--- mem x xs = not (null xs) && (x == head xs || mem x (tail xs))
+-- mem x []      =  False
+-- mem x (y:ys)  =  x == y || mem x ys
 
-memPrimitives :: [Prim]
-memPrimitives =
+memBackground :: [Prim]
+memBackground =
   [ pr False
-  , pr True
-  , pr ([] :: [Int])
-  , prim ":" ((:) :: Int -> [Int] -> [Int])
-  , prim "null" (null :: [Int] -> Bool)
   , prim "==" ((==) :: Int -> Int -> Bool)
-  , prim "not" (not :: Bool -> Bool)
   , prim "&&" ((&&) :: Bool -> Bool -> Bool)
   , prim "||" ((||) :: Bool -> Bool -> Bool)
-  , prim "head" (head :: [Int] -> Int)
-  , prim "tail" (tail :: [Int] -> [Int])
   ]
 
-setSpec :: ([Int] -> Bool) -> Bool
-setSpec set  =  and [ set []     ==  True
-                    , set [0]    ==  True
-                    , set [0,0]  ==  False
-                    , set [0,1]  ==  True
-                    , set [0,1,2] == True
-                    , set [0,0,1] == False
-                    , set [0,1,0] == False
-                    , set [0,1,1] == False ]
+sub :: [Int] -> [Int] -> Bool
+sub []      []     =  True
+sub []      [0,1]  =  True
+sub [0]     [0,1]  =  True
+sub [1]     [0,1]  =  True
+sub [2]     [0,1]  =  False
+sub [0,1]   [0,1]  =  True
+sub [1,0]   [0,1]  =  False
+sub [0,2]   [0,1]  =  False
+sub [2,0]   [0,1]  =  False
 
--- hoping for something like
--- set xs = null xs || not (elem (head xs) (tail xs)) && set (tail xs)
+-- Hoping for something like:
+-- sub :: [Int] -> [Int] -> Bool
+-- sub []     _       =  True
+-- sub _      []      =  False
+-- sub (x:xs) (y:ys)  =  x == y && sub xs ys || sub (x:xs) ys
 
-setPrimitives :: [Prim]
-setPrimitives =
-  [ prim "null" (null :: [Int] -> Bool)
+subBackground :: [Prim]
+subBackground  =
+  [ pr True
   , pr False
-  , pr True
-  , pr ([] :: [Int])
   , prim ":" ((:) :: Int -> [Int] -> [Int])
-  , prim "not" (not :: Bool -> Bool)
+  , prim "==" ((==) :: Int -> Int -> Bool)
   , prim "&&" ((&&) :: Bool -> Bool -> Bool)
   , prim "||" ((||) :: Bool -> Bool -> Bool)
-  , prim "head" (head :: [Int] -> Int)
-  , prim "tail" (tail :: [Int] -> [Int])
+  ]
+
+set :: [Int] -> Bool
+set []       =  True
+set [0]      =  True
+set [0,0]    =  False
+set [0,1]    =  True
+set [0,1,2]  =  True
+set [0,0,1]  =  False
+set [0,1,0]  =  False
+set [0,1,1]  =  False
+
+-- hoping for something like
+-- set []      =  True
+-- set (x:xs)  =  not (elem x xs) && set xs
+
+setBackground :: [Prim]
+setBackground =
+  [ prim "not" (not :: Bool -> Bool)
+  , prim "&&" ((&&) :: Bool -> Bool -> Bool)
   , prim "elem" (elem :: Int -> [Int] -> Bool)
   ]
 
-takeSpec :: (Int -> [Int] -> [Int]) -> Bool
-takeSpec take  =  and [ take 0 []     ==  []
-                      , take 1 []     ==  []
-                      , take 0 [0,1]  ==  []
-                      , take 1 [0,1]  ==  [0]
-                      , take 2 [0,1]  ==  [0,1]
-                      , take 3 [0,1]  ==  [0,1] ]
+take :: Int -> [A] -> [A]
+take 0 []     =  []
+take 1 []     =  []
+take 0 [x,y]  =  []
+take 1 [x,y]  =  [x]
+take 2 [x,y]  =  [x,y]
+take 3 [x,y]  =  [x,y]
 
 -- hoping for something like
--- take n xs = if n==0 || null xs then [] else head xs : take (dec n) (tail xs)
+-- take 0 xs      =  []
+-- take x []      =  []
+-- take x (y:ys)  =  y : take (x-1) ys
 
-takePrimitives :: [Prim]
-takePrimitives =
+takeBackground :: [Prim]
+takeBackground =
   [ pr (0 :: Int)
-  , pr ([] :: [Int])
-  , prim "null" (null :: [Int] -> Bool)
-  , prim "==" ((==) :: Int -> Int -> Bool)
-  , prim "||" ((||) :: Bool -> Bool -> Bool)
-  , prim "dec" ((\n -> n-1) :: Int -> Int)
-  , prim ":" ((:) :: Int -> [Int] -> [Int])
-  , prim "head" (head :: [Int] -> Int)
-  , prim "tail" (tail :: [Int] -> [Int])
+  , pr (1 :: Int)
+  , pr ([] :: [A])
+  , prim "-" ((-) :: Int -> Int -> Int)
+  , prim ":" ((:) :: A -> [A] -> [A])
   ]
 
-dropSpec :: (Int -> [Int] -> [Int]) -> Bool
-dropSpec drop  =  and [ drop 0 []     ==  []
-                      , drop 1 []     ==  []
-                      , drop 0 [0,1]  ==  [0,1]
-                      , drop 1 [0,1]  ==  [1]
-                      , drop 2 [0,1]  ==  []
-                      , drop 3 [0,1]  ==  [] ]
+drop :: Int -> [A] -> [A]
+drop 0 []     =  []
+drop 1 []     =  []
+drop 0 [x,y]  =  [x,y]
+drop 1 [x,y]  =  [y]
+drop 2 [x,y]  =  []
+drop 3 [x,y]  =  []
 
 -- hoping for something like
--- drop n xs = if n==0 || null xs then xs else drop (dec n) (tail xs)
+-- drop 0 xs  =  xs
+-- drop x []  =  []
+-- drop x (y:ys)  =  drop (x-1) ys
 
-dropPrimitives :: [Prim]
-dropPrimitives =
+dropBackground :: [Prim]
+dropBackground =
   [ pr (0 :: Int)
-  , pr ([] :: [Int])
-  , prim ":" ((:) :: Int -> [Int] -> [Int])
-  , prim "null" (null :: [Int] -> Bool)
-  , prim "==" ((==) :: Int -> Int -> Bool)
-  , prim "||" ((||) :: Bool -> Bool -> Bool)
-  , prim "dec" ((\n -> n-1) :: Int -> Int)
-  , prim "tail" (tail :: [Int] -> [Int])
+  , pr (1 :: Int)
+  , pr ([] :: [A])
+  , prim "-" ((-) :: Int -> Int -> Int)
   ]
 
 main :: IO ()
 main = do
-  conjureFromSpecWith args{maxSize=15} "sum" sumSpec sumPrimitives
-  conjureFromSpecWith args{maxSize=15} "app" appSpec appPrimitives
-  conjureFromSpecWith args{maxSize=15} "mem" memSpec memPrimitives
-  conjureFromSpecWith args{maxSize=15} "set" setSpec setPrimitives
-  conjureFromSpecWith args{maxSize=20} "take" takeSpec takePrimitives
-  conjureFromSpecWith args{maxSize=15} "drop" dropSpec dropPrimitives
+  conjure "sum" sum sumBackground
+  conjure "app" app appBackground
+  conjure "mem" mem memBackground
+  conjureWith args{maxSize=12,showCandidates=True} "sub" sub subBackground
+  conjure "set" set setBackground
+  conjure "take" take takeBackground
+  conjure "drop" drop dropBackground
 
