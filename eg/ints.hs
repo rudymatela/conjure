132dd12f4fccae1b7fc518783f7230638350b73e
Rudy Matela <rudy@matela.com.br>
2025-05-19 17:23:37 +0200

    eg/ints: simplify

--- eg/ints.hs
+++ eg/ints.hs
@@ -3,6 +3,7 @@
 -- Copyright (C) 2021-2025 Rudy Matela
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import Conjure
+import Prelude hiding (sum, product)
 
 second :: [Int] -> Int
 second [x,y]  =  y
@@ -13,46 +14,46 @@ third :: [Int] -> Int
 third [x,y,z]  =  z
 third [x,y,z,w]  =  z
 
-sum' :: [Int] -> Int
-sum' [1]      =  1
-sum' [1,2]    =  3
-sum' [1,2,3]  =  6
+sum :: [Int] -> Int
+sum [1]  =  1
+sum [1,2]  =  3
+sum [1,2,3]  =  6
 
-product' :: [Int] -> Int
-product' [2,3]    =  6
-product' [3,3]    =  9
-product' [2,3,4]  =  24
+product :: [Int] -> Int
+product [1]  =  1
+product [1,2]  =  2
+product [1,2,3]  =  6
 
 main :: IO ()
 main = do
-  conjure "second"  (second :: [Int] -> Int)
+  conjure "second" second
     [ fun "null" (null :: [Int] -> Bool)
     , fun "head" (head :: [Int] -> Int)
     , fun "tail" (tail :: [Int] -> [Int])
     ]
 
-  conjure "third"   (third :: [Int] -> Int)
+  conjure "third" third
     [ fun "null" (null :: [Int] -> Bool)
     , fun "head" (head :: [Int] -> Int)
     , fun "tail" (tail :: [Int] -> [Int])
     ]
 
-  conjure "sum"     (sum' :: [Int] -> Int)
+  conjure "sum" sum
     [ con (0 :: Int)
     , con (1 :: Int)
     , fun "+" ((+) :: Int -> Int -> Int)
     , fun "*" ((*) :: Int -> Int -> Int)
     ]
 
-  conjure "product" (product' :: [Int] -> Int)
+  conjure "product" product
     [ con (0 :: Int)
     , con (1 :: Int)
     , fun "+" ((+) :: Int -> Int -> Int)
     , fun "*" ((*) :: Int -> Int -> Int)
     ]
 
-  conjure "sum"     (sum' :: [Int] -> Int) ingredientsWithFold
-  conjure "product" (product' :: [Int] -> Int) ingredientsWithFold
+  conjure "sum"     sum     ingredientsWithFold
+  conjure "product" product ingredientsWithFold
 
 ingredients :: [Ingredient]
 ingredients =
